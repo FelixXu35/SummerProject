80a98eb78d97ae0629767470ec2dc79d1b7c46c0
FelixXu35 <61252303+FelixXu35@users.noreply.github.com>
2020-07-01 13:09:06 +0100

    add a file named "functions" to store all homemade functions.

---
+++ functions.py
@@ -0,0 +1,6 @@
+## functions.py
+# This file is a part of summer profect
+# THis file is used to store all functions defined by writter himself.
+# Written by Xiaotian Xu(Felix), 1st July 2020.
+
+
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+## Main.py
+# This file is a part of summer project.
+# This file is used to call all other functions and output result.
+# There is no input.
+# Written by Xiaotian Xu(Felix), 1st July 2020.
+
 ## Import
 import qutip as qt
 import numpy as np
 import matplotlib.pyplot as plt
 
 ## Defination
+N = 1e15 # the number of twp-level particles
+Kc = 2 * np.pi * 0.18 # the cavity mode decay rate(MHz)
+Ks = 2 * np.pi * 0.11 # the spin dephasing rate (MHz)
