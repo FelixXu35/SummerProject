3cc7b5e344d0ccd9676f3dcf2cbe880e182e5b5b
FelixXu35 <61252303+FelixXu35@users.noreply.github.com>
2020-07-11 16:30:19 +0100

    i saw an oscillation

--- learn.py
+++ learn.py
@@ -2,9 +2,15 @@
 from qutip import *
 import numpy as np
 from qutip.piqs import *
+import scipy as sc
 
 ## test
 #A = coherent_dm(5, 9)
-a = 1
-a += 1
-print(a)
+rho = ket2dm(basis(2, 1))
+print(rho)
+sm = destroy(2)
+print((sm.dag() * sm * rho).tr())
+#col = np.linspace(1, 1e15, 1e15)
+#data = np.linspace(1, 1e15, 1e15)
+#c = sc.sparse.coo_matrix((data, (row, col)), shape=(1e15, 1e15))
+#print(c)
--- main.py
+++ main.py
@@ -19,12 +19,12 @@ from qutip.piqs import *
 ## Defination
 dim_tls = 100 # the number of twp-level particles
 dim_lit = int(300) # the dimension of the light field
-num_steps = 50 # the number of steps will be used in evolution
-Kc = 2 * sc.pi * 0.18 # the cavity mode decay rate (MHz)
-Ks = 2 * sc.pi * 0.11 # the spin dephasing rate (MHz)
-gs = 2 * sc.pi * 4.2e-8 # the single spin-photon coupling strength (MHz)
-wc = 1.45 * 1e3 # the cavity frequency (MHz)
-ws = 1.45 * 1e3 # the spin traqnsition frequency (MHz)
+num_steps = 100 # the number of steps will be used in evolution
+Kc = 0.005#2 * sc.pi# * 0.18 # the cavity mode decay rate (MHz)
+Ks = 0.05#2 * sc.pi# * 0.11 # the spin dephasing rate (MHz)
+gs = 0.1 * sc.pi# * 4.2e-8 # the single spin-photon coupling strength (MHz)
+wc = 2 * sc.pi# * 1.45e3 # the cavity frequency (MHz)
+ws = 2 * sc.pi# * 1.45e3 # the spin traqnsition frequency (MHz)
 
 ## Initialization
 step_index = 0
@@ -33,12 +33,12 @@ n_phot = [] # the number of pjotons in the light field
 
 ## Define the collective spin operators
 [jx, jy, jz] = jspin(1) 
-jp = jspin(1, "+")
-jm = jp.dag()
+jm = destroy(2)
+jp = jm.dag()
 a = destroy(dim_lit)
 
 ## the initialization quantum state
-rho_tls = dicke(1, 0.5, 0.5)
+rho_tls = ket2dm(basis(2, 1))
 rho_phot = ket2dm(basis(dim_lit, 0))
 rho = tensor(rho_tls, rho_phot)
 
@@ -46,11 +46,11 @@ rho = tensor(rho_tls, rho_phot)
 nphot_tot = tensor(qeye(2), a.dag() * a) # number operator
 a_tot = tensor(qeye(2), a)
 jz_tot = tensor(jz, qeye(dim_lit))
-jp_tot = tensor(jp, qeye(dim_lit))
-jm_tot = tensor(jm, qeye(dim_lit))
+jp_tot = tensor(create(2), qeye(dim_lit))
+jm_tot = tensor(destroy(2), qeye(dim_lit))
 
 ## Time evolution
-t = np.linspace(0, 10, num_steps)
+t = np.linspace(0, 25, num_steps)
 step_length = t[1] - t[0]
 
 ## The RK2 method, which is realized with a for loop
@@ -60,19 +60,21 @@ for step_index in range(num_steps):
         rho_der = 0
 
         ## The number of excited atoms
-        N = (np.trace(rho * jz_tot) + 0.5) * dim_tls 
+        rho0 = rho.ptrace(0)
+        rho1 = rho.ptrace(1)
+        N = (jp * jm * rho0).tr() #* dim_tls# + 0.5) * dim_tls
         print(N)
         N = N.real
 
         ## The Hamiltonian
-        H = wc * a_tot.dag() * a_tot + ws * jz_tot + np.sqrt(N) * gs * (jp_tot * a_tot + jm_tot * a_tot.dag())
+        H = wc * a_tot.dag() * a_tot + ws * jm_tot.dag() * jm_tot + np.sqrt(1) * gs * (jp_tot * a_tot + jm_tot * a_tot.dag())
         rho_der -= 1j * commutator(H, rho)
 
         ## Cavity decay
         rho_der += Kc * (a_tot * rho * a_tot.dag() - 0.5 * a_tot.dag() * a_tot * rho - 0.5 * rho * a_tot.dag() * a_tot)
 
         ## Spin dephasing
-        rho_der += Ks * (jz_tot * rho * jz_tot.dag() - 0.5 * jz_tot.dag() * jz_tot * rho - 0.5 * rho * jz_tot.dag() * jz_tot)
+        rho_der += Ks * (jz_tot * rho * jz_tot - 0.5 * jz_tot * jz_tot * rho - 0.5 * rho * jz_tot * jz_tot)
         
         if step_index2 == 0:
             rho += rho_der * step_length # Predictor
@@ -80,8 +82,10 @@ for step_index in range(num_steps):
 
         if step_index2 == 1:
             rho = rho_dash + 0.5 * rho_der * step_length # Corrector
-            P = np.trace(rho * nphot_tot)
+            P = (a.dag() * a * rho1).tr()
+            P = P / rho0.tr()
             P = P.real
+            print(P)
             n_phot.append(P)
             n_exc.append(N)
 
